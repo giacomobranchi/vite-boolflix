0669e3a6412dab8c38a0d72990d9ed22b86c78cb
giacomobranchi <branchigiacomo28@gmail.com>
2023-09-28 16:20:47 +0200

    add milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
-<script setup>
-
+<script>
+import { state } from './state'
+export default {
+  data() {
+    return {
+      state
+    }
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <input v-model="state.userInput" type="text">
+  <button @click="state.fetchFilm(state.base_url)">Cerca</button>
+  <ol>
+    <li v-for="film in state.films">
+      <ul>
+        <li>{{ film.title }}</li>
+        <li>{{ film.original_title }}</li>
+        <li>{{ film.vote_average }}</li>
+        <li>{{ film.original_language }}</li>
+      </ul>
 
-<style scoped></style>
+    </li>
+  </ol>
+</template>
+
+<style scoped>
+li {
+  padding: 1rem 0;
+}
+</style>
--- src/state.js
+++ src/state.js
@@ -1,5 +1,17 @@
 import { reactive } from 'vue'
 import axios from 'axios'
 export const state = reactive({
+    base_url: 'https://api.themoviedb.org/3/search/movie?api_key=dd1e2153f10ecfa1cb72fcb0726e2452',
+    films: [],
+    userInput: '',
+
+
+    fetchFilm(url) {
+        axios.get(url + '&query=' + this.userInput)
+            .then(response => {
+                console.log(response);
+                this.films = response.data.results
+            })
+    },
 
 })
