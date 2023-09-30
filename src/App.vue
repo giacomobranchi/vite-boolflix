4db77777bbe02df1b28c836b6f4fa7afbc5765a6
giacomobranchi <branchigiacomo28@gmail.com>
2023-09-30 16:43:45 +0200

    add milestone 2

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,20 @@ export default {
 
 <template>
   <input v-model="state.userInput" type="text">
-  <button @click="state.fetchFilm(state.base_url)">Cerca</button>
+  <button @click="state.fetchFilm(state.film_url); state.fetchSeries(state.series_url)">Cerca</button>
   <ol>
-    <li v-for="film in state.films">
+    <li v-for="film in  state.films ">
       <ul>
-        <li>{{ film.title }}</li>
-        <li>{{ film.original_title }}</li>
+        <li v-if="film.title">{{ film.title }}</li>
+        <li v-else>{{ film.name }}</li>
+        <li v-if="film.original_title">{{ film.original_title }}</li>
+        <li v-else>{{ film.original_name }}</li>
         <li>{{ film.vote_average }}</li>
-        <li>{{ film.original_language }}</li>
+        <li><img :src="state.imgUrl + film.poster_path" alt=""></li>
+        <li>
+          <img :src="'https://flagcdn.com/16x12/' + `${film.original_language}` + '.png'" srcset="https://flagcdn.com/32x24/af.png 2x,
+          https://flagcdn.com/48x36/af.png 3x" width="16" height="12" :alt="film.original_language">
+        </li>
       </ul>
 
     </li>
--- src/state.js
+++ src/state.js
@@ -1,16 +1,38 @@
 import { reactive } from 'vue'
 import axios from 'axios'
 export const state = reactive({
-    base_url: 'https://api.themoviedb.org/3/search/movie?api_key=dd1e2153f10ecfa1cb72fcb0726e2452',
+    film_url: 'https://api.themoviedb.org/3/search/movie?api_key=dd1e2153f10ecfa1cb72fcb0726e2452',
+    series_url: 'https://api.themoviedb.org/3/search/tv?api_key=dd1e2153f10ecfa1cb72fcb0726e2452',
     films: [],
     userInput: '',
+    imgUrl: 'https://image.tmdb.org/t/p/w342/',
+    filmPosters: [],
+
 
 
     fetchFilm(url) {
+        this.films = []
         axios.get(url + '&query=' + this.userInput)
             .then(response => {
-                console.log(response);
-                this.films = response.data.results
+                //console.log(response);
+                this.films.push(...response.data.results)
+                console.log(this.films);
+            })
+    },
+    fetchSeries(url) {
+        axios.get(url + '&query=' + this.userInput)
+            .then(response => {
+                //console.log(response);
+                this.films.push(...response.data.results)
+                console.log(this.films)
+            })
+    },
+    fetchImg(url) {
+        axios.get(this.imgUrl + url)
+            .then(response => {
+                //console.log(response);
+                this.filmPosters = response.data.results.poster_path
+
             })
     },
 
