dc717e8be22b3bf1fc3019b022b2fd168927886b
giacomobranchi <branchigiacomo28@gmail.com>
2023-10-01 11:48:01 +0200

    add milestone 4

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<script>
+import { state } from '../state.js'
+export default {
+    data() {
+        return {
+            state,
+            name: 'AppHeader'
+        }
+    }
+
+}
+</script>
+        
+<template>
+    <div class="background">
+        <div class="container d-flex justify-content-between align-items-center">
+            <img height="60" src="../assets/img/logo.png" alt="">
+            <div>
+                <input v-model="state.userInput" type="text">
+                <button @click="state.fetchFilm(state.film_url); state.fetchSeries(state.series_url)">Cerca</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.background {
+    background-color: darkslategray;
+    box-shadow: 0 0 4px;
+
+    & .container {
+        padding: 1rem 0;
+
+        & h2 {
+            color: red;
+        }
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './assets/scss/style.scss'
 import App from './App.vue'
+import Bootstrap from '../node_modules/bootstrap/scss/bootstrap.scss'
 
 createApp(App).mount('#app')
